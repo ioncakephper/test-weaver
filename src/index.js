b83f628b930d9c4f27420ed35df45d334d76912f
Ion Gireada <ioncakephper@users.noreply.github.com>
2025-07-07 13:32:04 +0300

    fix: :zap: create better documentation

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 module.exports =
   /**
-   * Generates a configuration object for a file watcher.
+   * Generates a configuration object for a file watcher, and shows how to use it.
+   * This configuration is used to watch for changes in JavaScript, Markdown, and JSON files
+   * and automatically run ESLint and Prettier to fix and format the code.
+   * It also ignores certain directories like `node_modules`, `dist`, `coverage`, and `.git`.
    * @returns {object} The configuration object.
    */
   () => {
